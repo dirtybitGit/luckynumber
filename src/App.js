db75c5daf7e9eac10fed38f02200f1b8ac5d78b2
dirtybit <dirtybit@hotmail.com>
2020-02-26 15:19:09 +0900

    QR기능 테스트중

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React,{Component} from 'react';
-import './App.css';
-import Bottom from './component/Bottom';
-import NumberList from './component/NumberList';
+import React, { Component } from "react";
+import QrReader from "react-qr-reader";
+import "./App.css";
+import Bottom from "./component/Bottom";
+import NumberList from "./component/NumberList";
 
 class App extends Component {
-  
   LineCount = 5;
 
   state = {
-    numbers : [
+    numbers: [
       // {
       //   key : 0,
       //   num1 : '1',
@@ -18,99 +18,123 @@ class App extends Component {
       //   num5 : '5',
       //   num6 : '6',
       // }
-    ]
-  }
+    ],
+    QRResult: "",
+    QRShow: false
+  };
 
   initArray = (tmpArray, iCount) => {
-    for(var iIndex = 0; iIndex < iCount; iIndex++) {
+    for (var iIndex = 0; iIndex < iCount; iIndex++) {
       tmpArray[iIndex] = 0;
     }
     return tmpArray;
   };
 
-  getOneNum = (iCount) => {
+  getOneNum = iCount => {
     var iRandNum = 0;
     var tmpArray = new Array(45);
 
-    tmpArray = this.initArray(tmpArray,45);
-    for(var iIndex = 0; iIndex < iCount; iIndex++) {
-      iRandNum = Math.floor(Math.random() * 45) ;
+    tmpArray = this.initArray(tmpArray, 45);
+    for (var iIndex = 0; iIndex < iCount; iIndex++) {
+      iRandNum = Math.floor(Math.random() * 45);
       tmpArray[iRandNum] = tmpArray[iRandNum] + 1;
     }
     var iMaxCnt = 0;
-    
-    for(iIndex = 0; iIndex < 45; iIndex++) {
-        if(tmpArray[iIndex] > iMaxCnt) {
-          iRandNum = iIndex;
-          iMaxCnt = tmpArray[iIndex];
-        }
+
+    for (iIndex = 0; iIndex < 45; iIndex++) {
+      if (tmpArray[iIndex] > iMaxCnt) {
+        iRandNum = iIndex;
+        iMaxCnt = tmpArray[iIndex];
+      }
     }
-    return iRandNum + 1;    
-  }
+    return iRandNum + 1;
+  };
 
   clearNumList = () => {
-
-    const {numbers} = this.state;
+    const { numbers } = this.state;
     // 상태 모두 삭제
     this.setState({
-      numbers : numbers.splice(0,numbers.length)
-    });    
-
-  }
+      numbers: numbers.splice(0, numbers.length)
+    });
+  };
 
   makeNumList = () => {
-
     this.clearNumList();
 
     var newNum = [];
-    
+
     var tmpNum = 0;
 
-
-    for(var iIndex = 0; iIndex < this.LineCount; iIndex++) {
-        var aryTmp = [];
-        for(var iIndex1 = 0; iIndex1 < 6; iIndex1++) {
+    for (var iIndex = 0; iIndex < this.LineCount; iIndex++) {
+      var aryTmp = [];
+      for (var iIndex1 = 0; iIndex1 < 6; iIndex1++) {
+        tmpNum = this.getOneNum(100000);
+        while (aryTmp.indexOf(tmpNum) !== -1) {
           tmpNum = this.getOneNum(100000);
-          while(aryTmp.indexOf(tmpNum) !== -1) {
-            tmpNum = this.getOneNum(100000);
-          }
-          aryTmp = aryTmp.concat(tmpNum);
         }
-        aryTmp = aryTmp.sort(function(a,b){
-          return a - b;
-        });
-        newNum = newNum.concat({
-            key : iIndex,
-            num1 : aryTmp[0], 
-            num2 : aryTmp[1], 
-            num3 : aryTmp[2], 
-            num4 : aryTmp[3],  
-            num5 : aryTmp[4],  
-            num6 : aryTmp[5]
-        })
+        aryTmp = aryTmp.concat(tmpNum);
+      }
+      aryTmp = aryTmp.sort(function(a, b) {
+        return a - b;
+      });
+      newNum = newNum.concat({
+        key: iIndex,
+        num1: aryTmp[0],
+        num2: aryTmp[1],
+        num3: aryTmp[2],
+        num4: aryTmp[3],
+        num5: aryTmp[4],
+        num6: aryTmp[5]
+      });
     }
 
     this.setState({
-      numbers : newNum
+      numbers: newNum
     });
-  }
-
+  };
 
   handleRemake = () => {
     this.makeNumList();
-  }
+  };
 
   componentDidMount() {
     this.handleRemake();
   }
 
+  onQRScan = () => {
+    this.setState({ QRShow: !this.state.QRShow });
+  };
+
+  QRHandleScan = data => {
+    if (data) {
+      this.setState({ QRResult: data });
+      console.log(data);
+    }
+  };
+
+  QRHandleErr = err => {
+    console.log(err);
+  };
+
   render() {
-    const {numbers} = this.state;
+    const { numbers, QRShow } = this.state;
 
     return (
       <div className="App">
-        <NumberList numbers={numbers} />
-        <Bottom onRemake={ this.handleRemake }/>
+        {QRShow ? (
+          <div>
+            <QrReader
+              delay={300}
+              onError={this.QRHandleErr}
+              onScan={this.QRHandleScan}
+              style={{ width: "100%" }}
+            />
+            <p>{this.state.QRResult}</p>
+          </div>
+        ) : (
+          <NumberList numbers={numbers} />
+        )}
+        <Bottom onRemake={this.handleRemake} onQRScan={this.onQRScan} />
       </div>
     );
   }
--- src/component/Bottom.js
+++ src/component/Bottom.js
@@ -1,16 +1,20 @@
-import React, {Component} from 'react';
-import './Bottom.css'
+import React, { Component } from "react";
+import "./Bottom.css";
 
 class Bottom extends Component {
-
-    render() {
-        const {onRemake} = this.props;
-        return (
-            <div>
-                <button className='Btn' name="remake" onClick={onRemake}>다시 만들기</button>
-            </div>
-        )
-    }
+  render() {
+    const { onRemake, onQRScan } = this.props;
+    return (
+      <div>
+        <button className="Btn" name="remake" onClick={onRemake}>
+          다시 만들기
+        </button>
+        <button className="Btn" name="qrscan" onClick={onQRScan}>
+          당첨번호 확인하기
+        </button>
+      </div>
+    );
+  }
 }
- 
+
 export default Bottom;
