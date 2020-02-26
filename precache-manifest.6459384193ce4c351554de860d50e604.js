32c01cb52461287154f662fe0a8ee53421c4ca70
dirtybit <dirtybit@hotmail.com>
2020-02-26 15:17:46 +0900

    Updates

---
+++ precache-manifest.6459384193ce4c351554de860d50e604.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b86bd57f8665ca8bf2eda9d79aad05cb",
+    "url": "/lucky/index.html"
+  },
+  {
+    "revision": "af4e3b960a88f5a54278",
+    "url": "/lucky/static/css/main.5a4684dd.chunk.css"
+  },
+  {
+    "revision": "f6621564105318e59d40",
+    "url": "/lucky/static/js/2.3cdf6d78.chunk.js"
+  },
+  {
+    "revision": "af4e3b960a88f5a54278",
+    "url": "/lucky/static/js/main.71474486.chunk.js"
+  },
+  {
+    "revision": "064b41afed23b1322232",
+    "url": "/lucky/static/js/runtime-main.30a2e4bf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lucky_number/precache-manifest.18a3d87baf4d374cabc5e3662dc06686.js"
+  "/lucky/precache-manifest.6459384193ce4c351554de860d50e604.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lucky_number/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lucky/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
