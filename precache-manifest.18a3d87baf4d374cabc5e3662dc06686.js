0d05019ae32f58e21a1cac2ee3ea196497948cf7
dirtybit <dirtybit@hotmail.com>
2020-02-26 15:11:18 +0900

    Updates

---
+++ precache-manifest.18a3d87baf4d374cabc5e3662dc06686.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c076fb1fb74011080e6a87799714fc15",
+    "url": "/lucky_number/index.html"
+  },
+  {
+    "revision": "ff48e6dad3c0792ef94a",
+    "url": "/lucky_number/static/css/main.5a4684dd.chunk.css"
+  },
+  {
+    "revision": "f6621564105318e59d40",
+    "url": "/lucky_number/static/js/2.3cdf6d78.chunk.js"
+  },
+  {
+    "revision": "ff48e6dad3c0792ef94a",
+    "url": "/lucky_number/static/js/main.ccf56121.chunk.js"
+  },
+  {
+    "revision": "427380f291683ad0daaf",
+    "url": "/lucky_number/static/js/runtime-main.40a56035.js"
+  }
+]);
